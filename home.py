e5a1d85c49cbc93e71a627b33bfadbce3961e22b
riskahidayahputri <riskahidayahputri21@gmail.com>
2024-05-15 17:12:58 +0700

    tugas uts

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : Riska Hidayah Putri')
+st.subheader('NIM : 312210102')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/riska.jpg", caption="Riska Hidayah P", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/thvriska/")
+st.link_button("Github", "https://github.com/riskahidayahputri")
